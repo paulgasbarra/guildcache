1658d2952922379ce025d74fff976a9e70d6fdda
Paul Gasbarra <paulgasbarra@gmail.com>
2023-07-21 09:03:40 -0400

    initial commit

---
+++ s_and_j_track/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SAndJTrackConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 's_and_j_track'
---
+++ s_and_j_track/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField()
+    linkedin = models.URLField()
+    resume_link = models.URLField()
+    lca_cert = models.BooleanField(default=False)
+    epa_608_cert = models.BooleanField(default=False)
+    s_j_cert = models.BooleanField(default=False)
+    class_site = models.CharField(max_length=200)
+    class_number = models.IntegerField()
+    class_date = models.DateField()
+
+class Employer(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    website = models.URLField()
+    primary_contact = models.CharField(max_length=200)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField()
+    linkedin = models.URLField()
+    job_listings = models.URLField()
+    company_size = models.IntegerField()
+    speciality = models.CharField(max_length=200)
+
+class Application(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    applied_date = models.DateField()
+    passed_date = models.DateField(null=True, blank=True)
+    hired_date = models.DateField(null=True, blank=True)
+    salary_or_hourly_rate = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+
+class Donor(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField()
+    linkedin = models.URLField()
+    website = models.URLField()
+    instagram = models.URLField()
+    tiktok = models.URLField()
+
