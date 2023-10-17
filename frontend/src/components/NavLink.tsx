4747589ed5a90455fdcd6dc10b556594f5f619ce
Paul Gasbarra <paulgasbarra@gmail.com>
2023-10-16 22:34:04 -0400

    shape up nav bar

--- frontend/src/components/NavLink.tsx
+++ frontend/src/components/NavLink.tsx
@@ -6,12 +6,9 @@ interface RouterLinkProps {
   endpoint: string;
 }
 
-export const RouterLink: React.FC<RouterLinkProps> = ({
-  children,
-  endpoint,
-}) => (
+export const NavLink: React.FC<RouterLinkProps> = ({ children, endpoint }) => (
   <Link
-    className="text-base text-primary-blue font-nav font-black rounded-lg p-3 text-sm hover:text-gray-500 hover:bg-gray-50"
+    className="flex items-center text-primary-blue font-nav font-black rounded-lg p-3 text-sm hover:text-gray-500 hover:bg-gray-50"
     to={endpoint}
   >
     {children}
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { RouterLink } from "./RouterLink";
+import { NavLink } from "./NavLink";
 import { useAuth } from "./AuthContext";
 import { logo } from "../assets/images";
 
 const Navbar: React.FC = () => {
   const { isAuthenticated } = useAuth();
   return (
-    <nav className="flex justify-center font-nav">
-      <div className="group relative flex items-center">
-        <RouterLink endpoint="/enroll">BECOME A STUDENT</RouterLink>
-        <RouterLink endpoint="/partners">EMPLOYMENT PARTNERSHIP</RouterLink>
-        <RouterLink endpoint="/">
+    <nav className="flex justify-center font-nav px-10">
+      <div className="group relative w-full flex justify-between">
+        <NavLink endpoint="/enroll">BECOME A STUDENT</NavLink>
+        <NavLink endpoint="/partners">EMPLOYMENT PARTNERSHIP</NavLink>
+        <NavLink endpoint="/">
           <img src={logo} />
-        </RouterLink>
-        <RouterLink endpoint="/classes">CIRRICULUM</RouterLink>
-        <RouterLink endpoint="/about">MEET THE TEAM</RouterLink>
-        <RouterLink endpoint="/donate">DONATE</RouterLink>
-        {isAuthenticated && <RouterLink endpoint="/login">Logout</RouterLink>}
+        </NavLink>
+        <NavLink endpoint="/classes">CIRRICULUM</NavLink>
+        <NavLink endpoint="/about">MEET THE TEAM</NavLink>
+        <NavLink endpoint="/donate">DONATE</NavLink>
+        {isAuthenticated && <NavLink endpoint="/login">Logout</NavLink>}
       </div>
     </nav>
   );
