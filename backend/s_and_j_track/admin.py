8104cfdb13dfe4c380ea910b256adaa2a7e45034
Paul Gasbarra <paulgasbarra@gmail.com>
2024-04-03 21:58:34 -0400

    add organization to models, create stacks+joules org, map current students to it

--- backend/s_and_j_track/admin.py
+++ backend/s_and_j_track/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student, Employer, Application, Donor, Cohort, Instructor
+from .models import Student, Employer, Application, Donor, Cohort, Instructor, Organization
 
 admin.site.register(Student)
 admin.site.register(Employer)
@@ -7,4 +7,4 @@ admin.site.register(Application)
 admin.site.register(Donor)
 admin.site.register(Cohort)
 admin.site.register(Instructor)
-
+admin.site.register(Organization)
