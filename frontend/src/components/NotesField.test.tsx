88bf557696f3781944d6e2bfde50e152df78bccb
Paul Gasbarra <paulgasbarra@gmail.com>
2024-05-08 21:10:11 -0400

    improve tests for notesfield

--- frontend/src/components/NotesField.test.tsx
+++ frontend/src/components/NotesField.test.tsx
@@ -10,20 +10,22 @@ describe("NotesField Component", () => {
 
   it("renders a label element", () => {
     render(<NotesField notes="" updateNote={vi.fn()} />);
-    expect(screen.getByText("Notes")).toBeInTheDocument();
+    expect(screen.getByText("Notes:")).toBeInTheDocument();
   });
 
   it("renders the value of the notes prop", () => {
-    render(<NotesField notes="" updateNote={vi.fn()} />);
+    render(<NotesField notes="Example Notes" updateNote={vi.fn()} />);
     expect(screen.getByRole("textbox")).toHaveValue("Example Notes");
   });
 
-  it("calls the onChange prop when the value changes", () => {
-    const onChange = vi.fn();
-    render(<NotesField notes="" updateNote={vi.fn()} />);
+  //test that clicking "save notes" button calls the updateNote prop with the current note value
+  it("calls the updateNote prop when the button is clicked", () => {
+    const updateNote = vi.fn();
+    render(<NotesField notes="" updateNote={updateNote} />);
     fireEvent.change(screen.getByRole("textbox"), {
       target: { value: "New Notes" },
     });
-    expect(onChange).toHaveBeenCalledWith("New Notes");
+    fireEvent.click(screen.getByRole("button"));
+    expect(updateNote).toHaveBeenCalledWith("New Notes");
   });
 });
